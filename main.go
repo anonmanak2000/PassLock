3e3768d04a2faf3456a3caa113cf9e367dbf9fab
Singh <319089@NTTDATA.COM>
2023-12-14 21:48:29 +0530

    Removed minor details and added comments

--- main.go
+++ main.go
@@ -21,9 +21,9 @@ const UserFile = "users.json"
 
 type EncryptedPasswordEntry struct {
 	Tag      string `json:"tag"`
-	Cipher   []byte
-	Username string
-	Nonce    [24]byte
+	Cipher   []byte `json:"cipher"`
+	Username string `json:"username"`
+	Nonce    [24]byte `json:"nonce"`
 }
 
 type User struct {
@@ -41,6 +41,48 @@ func NewPasswordManager(username string) *PasswordManager {
 	}
 }
 
+//Read Username from CLI
+func readUserName() (string, error) {
+	fmt.Printf("Enter Username: ")
+
+	var username string
+	_, err := fmt.Scan(&username)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(username), nil
+}
+
+//Read Password securely from CLI
+func readPassword() (string, error) {
+	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("")
+
+	return strings.TrimSpace(string(password)), nil
+}
+
+//Generate a strong password for user
+func generateStrongPassword() (string, error) {
+	const length = 32
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:'\",.<>/?`~"
+	password := make([]byte, length)
+
+	for i := range password {
+		randomIndex, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		password[i] = charset[randomIndex.Int64()]
+	}
+
+	return string(password), nil
+}
+
+//Hash Password entered by user
 func (pm *PasswordManager) hashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -49,6 +91,7 @@ func (pm *PasswordManager) hashPassword(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
+//Create a User password - First time using CLI
 func (pm *PasswordManager) createUser() error {
 	fmt.Print("Enter password for passlock: ")
 	password, err := readPassword()
@@ -81,10 +124,11 @@ func (pm *PasswordManager) createUser() error {
 	return nil
 }
 
-func (pm *PasswordManager) authenticateUser() bool {
+//Authenticate User - Whenever using CLI commands
+func (pm *PasswordManager) authenticateUser() bool,error {
 	users, err := pm.loadUsers()
 	if err != nil {
-		return false
+		return false, err
 	}
 
 	var storedPassword string
@@ -96,102 +140,24 @@ func (pm *PasswordManager) authenticateUser() bool {
 	}
 
 	if storedPassword == "" {
-		return false
+		return false, nil
 	}
 
 	fmt.Printf("Enter your password for %s: ", pm.Username)
 	password, err := readPassword()
 	if err != nil {
-		return false
+		return false, err
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(password))
 	if err != nil {
-		return false
+		return false, err
 	}
 
-	return true
+	return true, nil
 }
 
-func (pm *PasswordManager) createStrongPassword(tag, password, username string, generate bool) error {
-	if generate {
-		strongpasword, err := generateStrongPassword()
-		if err != nil {
-			fmt.Printf("Error while generating strong password. \n")
-			return err
-		}
-		password = strongpasword
-		fmt.Printf("Generated strong password: %s\n", strongpasword)
-
-	} else if password == "" {
-		return fmt.Errorf("password is required")
-	}
-
-	nonce := generateNonce()
-	encryptedPassword, err := pm.encryptPassword(password, nonce)
-	if err != nil {
-		return err
-	}
-
-	entry := EncryptedPasswordEntry{
-		Tag:      tag,
-		Cipher:   encryptedPassword,
-		Username: username,
-		Nonce:    *nonce,
-	}
-
-	err = pm.savePasswordEntry(entry)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Strong password created successfully.")
-	return nil
-}
-
-func (pm *PasswordManager) savePasswordEntry(entry EncryptedPasswordEntry) error {
-	entries, err := pm.loadPasswordEntries()
-	if err != nil {
-		return err
-	}
-
-	entries = append(entries, entry)
-
-	return pm.savePasswordEntries(entries)
-}
-
-func (pm *PasswordManager) savePasswordEntries(entries []EncryptedPasswordEntry) error {
-	fileContents, err := json.MarshalIndent(entries, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(PasswordFile, fileContents, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (pm *PasswordManager) loadPasswordEntries() ([]EncryptedPasswordEntry, error) {
-	fileContents, err := os.ReadFile(PasswordFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return []EncryptedPasswordEntry{}, nil
-		}
-		return nil, err
-	}
-
-	var entries []EncryptedPasswordEntry
-	err = json.Unmarshal(fileContents, &entries)
-	if err != nil {
-		return nil, err
-	}
-
-	return entries, nil
-}
-
+//Read the Secret key from file if exists otherwise create a secret key and store it.
 func readOrCreateKey() [32]byte {
 	key, err := os.ReadFile(KeyFile)
 	if err != nil {
@@ -221,6 +187,7 @@ func readOrCreateKey() [32]byte {
 	return secretKey
 }
 
+//Generate a Random Key
 func generateRandomKey() [32]byte {
 	var key [32]byte
 	_, err := rand.Read(key[:])
@@ -231,6 +198,18 @@ func generateRandomKey() [32]byte {
 	return key
 }
 
+//Generate Nonce for encryption/decryption of password
+func generateNonce() *[24]byte {
+	var nonce [24]byte
+	_, err := rand.Read(nonce[:])
+	if err != nil {
+		fmt.Println("Error generating nonce:", err)
+		os.Exit(1)
+	}
+	return &nonce
+}
+
+//Encrypt the password
 func (pm *PasswordManager) encryptPassword(password string, nonce *[24]byte) ([]byte, error) {
 	secretKey := readOrCreateKey()
 
@@ -238,6 +217,7 @@ func (pm *PasswordManager) encryptPassword(password string, nonce *[24]byte) ([]
 	return encrypted, nil
 }
 
+//Decrypt the password
 func (pm *PasswordManager) decryptPassword(cipher []byte, nonce *[24]byte) (string, error) {
 	secretKey := readOrCreateKey()
 
@@ -249,38 +229,46 @@ func (pm *PasswordManager) decryptPassword(cipher []byte, nonce *[24]byte) (stri
 	return string(decrypted), nil
 }
 
-func (pm *PasswordManager) saveUsers(users []User) error {
-	fileContents, err := json.MarshalIndent(users, "", "  ")
+
+//Operations:
+//1. Add Password With Tag
+func (pm *PasswordManager) addPasswordWithTag(tag, password string, generate bool) error {
+	if generate {
+		strongpasword, err := generateStrongPassword()
+		if err != nil {
+			fmt.Printf("Error while generating strong password. \n")
+			return err
+		}
+		password = strongpasword
+		fmt.Printf("Generated strong password: %s\n", strongpasword)
+
+	} else if password == "" {
+		return fmt.Errorf("password is required")
+	}
+
+	nonce := generateNonce()
+	encryptedPassword, err := pm.encryptPassword(password, nonce)
 	if err != nil {
 		return err
 	}
 
-	err = os.WriteFile(UserFile, fileContents, 0644)
+	entry := EncryptedPasswordEntry{
+		Tag:      tag,
+		Cipher:   encryptedPassword,
+		Username: pm.Username,
+		Nonce:    *nonce,
+	}
+
+	err = pm.savePasswordEntry(entry)
 	if err != nil {
 		return err
 	}
 
+	fmt.Println("Strong password created successfully.")
 	return nil
 }
 
-func (pm *PasswordManager) loadUsers() ([]User, error) {
-	fileContents, err := os.ReadFile(UserFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return []User{}, nil
-		}
-		return nil, err
-	}
-
-	var users []User
-	err = json.Unmarshal(fileContents, &users)
-	if err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
-
+//2. Get All Tags
 func (pm *PasswordManager) getAllTags() {
 	entries, err := pm.loadPasswordEntries()
 	if err != nil {
@@ -294,34 +282,39 @@ func (pm *PasswordManager) getAllTags() {
 	}
 }
 
-func (pm *PasswordManager) addPasswordWithTag(tag, password, username string) error {
-	if password == "" {
-		return fmt.Errorf("password is required")
-	}
-
-	nonce := generateNonce()
-	encryptedPassword, err := pm.encryptPassword(password, nonce)
+//3. Delete Password with Tag
+func (pm *PasswordManager) deletePasswordWithTag(tag string) error {
+	entries, err := pm.loadPasswordEntries()
 	if err != nil {
 		return err
 	}
 
-	entry := EncryptedPasswordEntry{
-		Tag:      tag,
-		Cipher:   encryptedPassword,
-		Username: username,
-		Nonce:    *nonce,
+	var newEntries []EncryptedPasswordEntry
+	deleted := false
+
+	for _, entry := range entries {
+		if entry.Tag == tag && entry.Username == pm.Username {
+			deleted = true
+			continue
+		}
+		newEntries = append(newEntries, entry)
 	}
 
-	err = pm.savePasswordEntry(entry)
+	if !deleted {
+		return fmt.Errorf("password with tag '%s' not found", tag)
+	}
+
+	err = pm.savePasswordEntries(newEntries)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("Password added successfully.")
+	fmt.Println("Password deleted successfully.")
 	return nil
 }
 
-func (pm *PasswordManager) updatePasswordWithTag(tag, password, username string, generate bool) error {
+//4. Update Password with Tag
+func (pm *PasswordManager) updatePasswordWithTag(tag, password string, generate bool) error {
 	if generate {
 		strongpassword, err := generateStrongPassword()
 		if err != nil {
@@ -344,7 +337,7 @@ func (pm *PasswordManager) updatePasswordWithTag(tag, password, username string,
 	entry := EncryptedPasswordEntry{
 		Tag:      tag,
 		Cipher:   encryptedPassword,
-		Username: username,
+		Username: pm.Username,
 		Nonce:    *nonce,
 	}
 
@@ -357,37 +350,8 @@ func (pm *PasswordManager) updatePasswordWithTag(tag, password, username string,
 	return nil
 }
 
-func (pm *PasswordManager) deletePasswordWithTag(tag, username string) error {
-	entries, err := pm.loadPasswordEntries()
-	if err != nil {
-		return err
-	}
-
-	var newEntries []EncryptedPasswordEntry
-	deleted := false
-
-	for _, entry := range entries {
-		if entry.Tag == tag && entry.Username == username {
-			deleted = true
-			continue
-		}
-		newEntries = append(newEntries, entry)
-	}
-
-	if !deleted {
-		return fmt.Errorf("password with tag '%s' not found", tag)
-	}
-
-	err = pm.savePasswordEntries(newEntries)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Password deleted successfully.")
-	return nil
-}
-
-func (pm *PasswordManager) getPasswordWithTag(tag, username string) {
+//5. Get Password with a Tag
+func (pm *PasswordManager) getPasswordWithTag(tag string) {
 	entries, err := pm.loadPasswordEntries()
 	if err != nil {
 		fmt.Println("Error getting password:", err)
@@ -395,7 +359,7 @@ func (pm *PasswordManager) getPasswordWithTag(tag, username string) {
 	}
 
 	for _, entry := range entries {
-		if entry.Tag == tag && entry.Username == username {
+		if entry.Tag == tag && entry.Username == pm.Username {
 			password, err := pm.decryptPassword(entry.Cipher, &entry.Nonce)
 			if err != nil {
 				fmt.Println("Error getting password:", err)
@@ -409,78 +373,148 @@ func (pm *PasswordManager) getPasswordWithTag(tag, username string) {
 	fmt.Printf("Password for tag '%s' not found\n", tag)
 }
 
-func readUserName() (string, error) {
-	fmt.Printf("Enter Username: ")
-
-	var username string
-	_, err := fmt.Scan(&username)
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(username), nil
-}
-
-func readPassword() (string, error) {
-	password, err := term.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil {
-		return "", err
+/*
+func (pm *PasswordManager) addPasswordWithTag(tag, password string) error {
+	if password == "" {
+		return fmt.Errorf("password is required")
 	}
 
-	fmt.Println("") // Print a newline after password input for better formatting
+	nonce := generateNonce()
+	encryptedPassword, err := pm.encryptPassword(password, nonce)
+	if err != nil {
+		return err
+	}
 
-	return strings.TrimSpace(string(password)), nil
+	entry := EncryptedPasswordEntry{
+		Tag:      tag,
+		Cipher:   encryptedPassword,
+		Username: pm.Username,
+		Nonce:    *nonce,
+	}
+
+	err = pm.savePasswordEntry(entry)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Password added successfully.")
+	return nil
+}
+*/
+
+//File Operations:
+//Updating Password Entries
+func (pm *PasswordManager) savePasswordEntry(entry EncryptedPasswordEntry) error {
+	entries, err := pm.loadPasswordEntries()
+	if err != nil {
+		return err
+	}
+
+	entries = append(entries, entry)
+
+	return pm.savePasswordEntries(entries)
 }
 
-func generateStrongPassword() (string, error) {
-	const length = 32
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:'\",.<>/?`~"
-	password := make([]byte, length)
+//Saving Updated Password Entries in the file
+func (pm *PasswordManager) savePasswordEntries(entries []EncryptedPasswordEntry) error {
+	fileContents, err := json.MarshalIndent(entries, "", "  ")
+	if err != nil {
+		return err
+	}
 
-	for i := range password {
-		randomIndex, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
-		if err != nil {
-			return "", err
+	err = os.WriteFile(PasswordFile, fileContents, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//Load Password Entries from file
+func (pm *PasswordManager) loadPasswordEntries() ([]EncryptedPasswordEntry, error) {
+	fileContents, err := os.ReadFile(PasswordFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []EncryptedPasswordEntry{}, nil
 		}
-		password[i] = charset[randomIndex.Int64()]
+		return nil, err
 	}
 
-	return string(password), nil
+	var entries []EncryptedPasswordEntry
+	err = json.Unmarshal(fileContents, &entries)
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, nil
 }
 
-func generateNonce() *[24]byte {
-	var nonce [24]byte
-	_, err := rand.Read(nonce[:])
+//Save Users in the file
+func (pm *PasswordManager) saveUsers(users []User) error {
+	fileContents, err := json.MarshalIndent(users, "", "  ")
 	if err != nil {
-		fmt.Println("Error generating nonce:", err)
-		os.Exit(1)
+		return err
 	}
-	return &nonce
+
+	err = os.WriteFile(UserFile, fileContents, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//Load Users from the file
+func (pm *PasswordManager) loadUsers() ([]User, error) {
+	fileContents, err := os.ReadFile(UserFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []User{}, nil
+		}
+		return nil, err
+	}
+
+	var users []User
+	err = json.Unmarshal(fileContents, &users)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
 
 func printUsage() {
 	fmt.Println("Usage:")
 	fmt.Println("passlock <operation> [options]")
 	fmt.Println("Operations:")
-	fmt.Println("-create-strong-password <tag> [-password=<password> | -generate]")
-	fmt.Println("	- Create a strong password for the specified tag.")
-	fmt.Println("	Options:")
-	fmt.Println("		-password=<password>  Specify the password.")
-	fmt.Println("		-generate             Generate a strong password.")
+	fmt.Println("-add-password <tag> [-password=<password> | -generate]")
+	fmt.Println("Create/Add a strong password for the specified tag.")
+	fmt.Println("Options:")
+	fmt.Println("	-password=<password>  Specify the password.")
+	fmt.Println("	-generate             Generate a strong password.")
+
+	/*
 	fmt.Println("-add-password <tag> -password=<password>")
-	fmt.Println("	- Add a password for the specified tag.")
+	fmt.Println("Add a password for the specified tag.")
+	*/
+
 	fmt.Println("-update-password <tag> [-password=<password> | -generate]")
-	fmt.Println("	- Update the password for the specified tag.")
-	fmt.Println("	Options:")
-	fmt.Println("		-password=<password>  Specify the new password.")
-	fmt.Println("		-generate             Generate a new strong password.")
+	fmt.Println("Update the password for the specified tag.")
+	fmt.Println("Options:")
+	fmt.Println("	-password=<password>  Specify the new password.")
+	fmt.Println("	-generate             Generate a new strong password.")
+
 	fmt.Println("-delete-password <tag>")
-	fmt.Println("	- Delete the password for the specified tag.")
+	fmt.Println("Delete the password for the specified tag.")
+
 	fmt.Println("-get-password <tag>")
-	fmt.Println("	- Get the password for the specified tag.")
+	fmt.Println("Get the password for the specified tag.")
+
 	fmt.Println("-get-tags")
-	fmt.Println("	- Get all tags.")
+	fmt.Println("Get all tags.")
+
 	fmt.Println("-help")
-	fmt.Println("	- Display this help message.")
+	fmt.Println("Display this help message.")
 }
 
 func main() {
@@ -496,19 +530,27 @@ func main() {
 	}
 
 	operation := os.Args[1]
+
 	pm := NewPasswordManager(username)
 
-	if !pm.authenticateUser() {
+	authenticated, err := pm.authenticateUser()
+
+	if(err != nil){
+		fmt.Println("Error while reading password: ", err)
+		return
+	}
+
+	if !authenticated {
 		fmt.Println("Creating a new user.")
 		err := pm.createUser()
 		if err != nil {
-			fmt.Println("Error creating user:", err)
+			fmt.Println("Error creating user: ", err)
 			return
 		}
 	}
 
 	switch operation {
-	case "-create-strong-password":
+	case "-add-password":
 		var tag, password string
 		var generate bool
 
@@ -518,10 +560,11 @@ func main() {
 		flagSet.BoolVar(&generate, "generate", false, "Generate a strong password.")
 		flagSet.Parse(os.Args[2:])
 
-		err := pm.createStrongPassword(tag, password, username, generate)
+		err := pm.addPasswordWithTag(tag, password, generate)
 		if err != nil {
-			fmt.Println("Error:", err)
+			fmt.Println("An error occured: ", err)
 		}
+		/*
 	case "-add-password":
 		var tag, password string
 
@@ -530,10 +573,11 @@ func main() {
 		flagSet.StringVar(&password, "password", "", "Specify the password.")
 		flagSet.Parse(os.Args[2:])
 
-		err := pm.addPasswordWithTag(tag, password, username)
+		err := pm.addPasswordWithTag(tag, password)
 		if err != nil {
-			fmt.Println("Error:", err)
+			fmt.Println("An error occured: ", err)
 		}
+		*/
 	case "-update-password":
 		var tag, password string
 		var generate bool
@@ -544,9 +588,9 @@ func main() {
 		flagSet.BoolVar(&generate, "generate", false, "Generate a new strong password.")
 		flagSet.Parse(os.Args[2:])
 
-		err := pm.updatePasswordWithTag(tag, password, username, generate)
+		err := pm.updatePasswordWithTag(tag, password, generate)
 		if err != nil {
-			fmt.Println("Error:", err)
+			fmt.Println("An error occured: ", err)
 		}
 	case "-delete-password":
 		var tag string
@@ -554,9 +598,9 @@ func main() {
 		flagSet.StringVar(&tag, "tag", "", "Specify the tag.")
 		flagSet.Parse(os.Args[2:])
 
-		err := pm.deletePasswordWithTag(tag, username)
+		err := pm.deletePasswordWithTag(tag)
 		if err != nil {
-			fmt.Println("Error:", err)
+			fmt.Println("An error occured: ", err)
 		}
 	case "-get-password":
 		var tag string
@@ -565,7 +609,7 @@ func main() {
 		flagSet.StringVar(&tag, "tag", "", "Specify the tag.")
 		flagSet.Parse(os.Args[2:])
 
-		pm.getPasswordWithTag(tag, username)
+		pm.getPasswordWithTag(tag)
 	case "-get-tags":
 		pm.getAllTags()
 	default:
